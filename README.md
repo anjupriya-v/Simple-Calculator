6d2f96ed6603a8cbb8c531ab4c6ded0f95cfce21
anjupriya-v <anjupriyachennai@gmail.com>
2022-11-11 17:05:25 +0530

    Files Modified

--- README.md
+++ README.md
@@ -1,16 +1,22 @@
 # simple-calculator
 
-## Live Demo:
-$ https://simple-calculator-application.herokuapp.com/
+## Tech Stack Used:
+
+- React.js
+
+## Demo Video:
+
+https://user-images.githubusercontent.com/84177086/201328830-58bf6b40-20ca-492c-af05-82bb1d03198b.mp4
+
+
 ## :point_down:Steps to initialize the project:
+
 
 - Clone the repository
 
 $ git clone https://github.com/anjupriya-v/simple-calculator.git
 
 - Redirect to the cloned repo directory
-
-$ cd simple-calculator
 
 - Install the dependencies
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import About from './About';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./css/index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import About from "./js/About";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 const Routing = () => {
   return (
@@ -13,13 +13,13 @@ const Routing = () => {
         <Route path="/about" component={About} />
       </Switch>
     </Router>
-  )
-}
+  );
+};
 ReactDOM.render(
   <React.StrictMode>
     <Routing />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
